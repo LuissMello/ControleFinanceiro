69efc7f0b258d7ce25cf3d1f0b0e60d90bdb54ef
Luis Henrique Nicoleli de Mello <lhnmello@hotmail.com>
2021-11-16 04:20:13 -0300

    Configurações de repository

---
+++ ControleFinanceiroApi/Dependency/DependencyConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using ControleFinanceiro.Core.Data;
+using ControleFinanceiro.Data.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.Api.Dependency
+{
+    public static class DependencyConfiguration
+    {
+        public static void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+            services.AddScoped<ILocalRepository, LocalRepository>();
+            services.AddScoped<ISaldoRepository, SaldoRepository>();
+            services.AddScoped<ITransacaoRepository, TransacaoRepository>();
+        }
+    }
+}
